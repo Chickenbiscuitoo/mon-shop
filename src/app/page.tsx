5dcd0ea3086e833a7627d170e8412ac256609f16
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2023-05-29 17:35:12 +0200

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import MonCard from '../components/MonCard'
 
 async function getMons() {
 	const res = await fetch(
-		'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0'
+		'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0'
 	)
 	return res.json()
 }
@@ -11,7 +11,7 @@ async function Home() {
 	const mons = await getMons()
 
 	return (
-		<div>
+		<div className="bg-red-200 grid grid-cols-4 w-full">
 			{mons.results.map((mon: any) => (
 				/* @ts-expect-error Async Server Component */
 				<MonCard name={mon.name} url={mon.url} />
--- src/components/MonCard.tsx
+++ src/components/MonCard.tsx
@@ -13,7 +13,18 @@ async function MonCard({ name, url }: MonCardProps) {
 
 	console.log(mon)
 
-	return <div>{name}</div>
+	return (
+		<div className="bg-blue-200 flex flex-col place-content-center place-items-center border-radius-assm">
+			<img src={mon.sprites.front_default} alt="" />
+			<h2>#{mon.id}</h2>
+			<h1>{name}</h1>
+			<span>
+				{mon.types.map((type: any) => (
+					<h2 className="inline">{type.type.name}</h2>
+				))}
+			</span>
+		</div>
+	)
 }
 
 export default MonCard
