bd6f7d7a0b024a2f9f25780044d1a17e0c41ddd9
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2023-06-01 16:59:57 +0200

    styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
 	return (
-		<header className="h-10 flex place-content-center place-items-center border-b-2 border-neutral pt-6 pb-6">
+		<header className="h-10 flex place-content-center place-items-center border-b-2 border-base-content pt-6 pb-6">
 			Header
 		</header>
 	)
