8f94147fc1e233df09b421932848d5e1a0fb18e0
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2023-06-07 18:54:03 +0200

    updated

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -15,9 +15,13 @@ function Pagination({ currPage, route, queryParams }: paginationProps) {
 	return (
 		<div className="join flex place-content-center place-items-center place-self-center w-full gap-2 m-2">
 			<Link href={prevLink}>
-				<button className="join-item btn">«</button>
+				<button className="join-item btn" disabled={currPage < 2}>
+					«
+				</button>
 			</Link>
-			<button className="join-item btn">Page {currPage}</button>
+			<button className="join-item btn cursor-default hover:bg-neutral w-1/4">
+				Page {currPage}
+			</button>
 			<Link href={nextLink}>
 				<button className="join-item btn">»</button>
 			</Link>
